d452b9ad5b84c2c730a5b88886f79a3ba049d755
wangshoudu <wangshoudu@yungengxin.com>
2020-11-28 10:48:05 +0800

    update

---
+++ doc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/lichaohui/Documents/VPN/ssr-n/depends/json-c/doc
+# Build directory: /Users/lichaohui/Documents/VPN/ssr-n/depends/json-c/doc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
